398d850fc75a8a39011b5450cf2632772a04e7c3
Soslan Khubulov <soslanx@gmail.com>
2014-01-13 18:45:57 +0400

    Preparing core layer

---
+++ src/core.js
@@ -0,0 +1,53 @@
+// Core layer
+function Element(args){
+	var self = this;
+	args=args?args:{};
+	args.tagName = args.tagName ? args.tagName : "div";
+	this.element = document.createElement(args.tagName);
+	this.$element = $(this.element);
+	this.tagName = args.tagName;
+	
+	this.element.className = "element";
+
+	this.container = this.element; // Temporary.
+
+	if(args.className){
+		this.element.className += " " + args.className;
+	}
+	if(args.onClick){
+		$(this.element).click(args.onClick);
+	}
+	if(args.onMouseDown){
+		$(this.element).mousedown(args.onMouseDown);
+	}
+}
+
+Element.prototype.append = function(element){
+	this.element.appendChild(element.element);
+	return this;
+}
+
+Element.prototype.prepend = function(element){
+	$(this.element).prepend(element.element);
+	return this;
+}
+
+Element.prototype.addClass = function(className){
+	this.element.classList.add(className);
+	return this;
+}
+
+Element.prototype.removeClass = function(className){
+	this.element.classList.remove(className);
+	return this;
+}
+
+// Text input
+
+function TextInputCore(args){
+	var self = this;
+	args = args?args:{};
+	Element.call(this,{
+		className:"text-input",
+	});
+}
