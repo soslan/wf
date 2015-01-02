89ae6a4688a86b759b7013b0d58f24b1d4c38618
Soslan Khubulov <soslanx@gmail.com>
2015-01-02 21:33:26 +0300

    TabView rewritten, Windows started

--- src/views.js
+++ src/views.js
@@ -1,147 +1,46 @@
-function TabView(args){
+function TabView(args, tabsArgs, bodiesArgs){
 	var self = this;
 	args=args?args:{};
+	tabsArgs = tabsArgs || {};
+	bodiesArgs = bodiesArgs || {};
 	Container.call(this,{
 		contentDirection:args.direction,
 		contentType:'blocks',
 	});
 	this.addClass('tab-view');
 
-	this.tabsContainer = new Container({
-		contentDirection:"horizontal",
-	});
+	this.tabs = new Toolbar(tabsArgs);
 
-	this.documentsContainer = new Container({
-		share:1,
-		contentType:"blocks",
-	});
-	this.tabs = [];
+	bodiesArgs.share = 1;
+	//bodiesArgs.contentType
+
+	this.bodies = new Containers(bodiesArgs);
+	//this.tabs = [];
 	this.activeTab;
 
-	this.tabsContainer.addClass('tab-view-tabs');
-	this.documentsContainer.addClass('tab-view-documents');
+	this.tabs.addClass('tab-view-tabs');
+	this.bodies.addClass('tab-view-bodies');
 
-	this.append(this.tabsContainer)
-		.append(this.documentsContainer);
 
-	this.addTab = function(newTabArgs){
-		newTabArgs = newTabArgs || {};
-		var tab = {};
-		this.tabs.push(tab);
-		tab.tabElement = new Toolbar();
-		tab.tabTitle = new Label();
-		tab.documentElement = new Container({
-			share:1,
-		});
-		tab.label = new Value();
+	Container.prototype.append.call(this, this.tabs);
+	Container.prototype.append.call(this, this.bodies);
 
-		tab.onActivate = newTabArgs.onActivate;
-		tab.onDeactivate = newTabArgs.onDeactivate;
-
-		tab.tabElement.addClass('tab-view-tab');
-		tab.documentElement.addClass('tab-view-document hidden');
-
-		tab.tabElement.addEventListener('click',function(){
-			tab.activate();
-		});
-
-		tab.label.addEventListener('change',function(data){
-			tab.tabTitle.setText(data.value);
-		});
-
-		tab.label.set({
-			value:newTabArgs.label || '',
-		});
-
-		tab.tabElement.append(tab.tabTitle);
-
-		self.tabsContainer.append(tab.tabElement);
-		self.documentsContainer.append(tab.documentElement);
-
-		tab.activate = function(){
-			if(typeof self.activeTab !== "undefined"){
-				self.activeTab.tabElement.element.classList.remove('active');
-				self.activeTab.documentElement.addClass('hidden');
-				if(typeof self.activeTab.onDeactivate == "function"){
-					self.activeTab.onDeactivate();
-				}
-			}
-			self.activeTab = tab;
-			tab.tabElement.element.classList.add('active');
-			tab.documentElement.removeClass('hidden');
-			if(typeof tab.onActivate == "function"){
-				tab.onActivate();
-			}
-		}
-
-		tab.close = function(){
-			var currentIndex = self.tabs.indexOf(tab);
-			self.tabsContainer.element.removeChild(tab.tabElement.element);
-			self.documentsContainer.element.removeChild(tab.documentElement.element);
-			if(tab == self.activeTab){
-				if(currentIndex + 1 == self.tabs.length){
-					if(self.tabs.length == 1){
-						self.activeTab = null;
-					}
-					else{
-						self.tabs[currentIndex - 1].activate();
-					}
-				}
-				else{
-						self.tabs[currentIndex + 1].activate();
-				}
-			}
-			self.tabs.splice(currentIndex, 1);
-			delete tab;
-		}
-
-		if(newTabArgs.closeable == true){
-			var closeElement = new Button({
-				icon:'times',
-				className:"quiet red",
-			});
-			tab.tabElement.append(closeElement);
-			closeElement.clickable({
-				onClick:function(e){
-					tab.close();
-					e.stopPropagation();
-					e.preventDefault();
-				}
-			});
-		}
-
-		if(newTabArgs.activate == true || this.activeTab == undefined){
-			if(typeof self.activeTab !== "undefined"){
-				self.activeTab.tabElement.element.classList.remove('active');
-				self.activeTab.documentElement.addClass('hidden');
-				if(typeof self.activeTab.onDeactivate == "function"){
-					self.activeTab.onDeactivate();
-				}
-			}
-			self.activeTab = tab;
-			tab.tabElement.element.classList.add('active');
-			tab.documentElement.removeClass('hidden');
-			if(typeof newTabArgs.ready == "function"){
-				newTabArgs.ready(tab);
-			}
-			/*if(typeof tab.onActivate == "function"){
-				tab.onActivate();
-			}*/
-
-		}
-		else{
-			if(typeof newTabArgs.ready == "function"){
-				newTabArgs.ready(tab);
-			}
-		}
-
-		
-
-		return tab;
-	};
 }
 
 TabView.prototype = Object.create(Container.prototype);
+
+TabView.prototype.append = function(container, handleArgs){
+	this.bodies.append(container);
+	if(handleArgs === undefined){
+		handleArgs = {};
+	}
+	else if (typeof handleArgs === "string"){
+		handleArgs = {
+			text:handleArgs,
+		};
+	}
+	this.tabs.append(container.getHandle(handleArgs));
+}
 
 
 function AccordionView(args){
@@ -227,4 +126,98 @@ AccordionView.prototype = Object.create(Container.prototype);
 	this.windows.push(newWindow);
 }*/
 
+
+function Windows(args){
+	var self = this;
+	args = args?args:{};
+	Container.call(this,args);
+	this.addClass('windows');
+	this.windows = [];
+	this.topZIndex = 200;
+	this.active;
+	//this.activeMaximizedContainer;
+}
+
+Windows.prototype = Object.create(Container.prototype);
+
+Windows.prototype.append = function(container){
+	var self = this;
+	var displayType;
+	if (!(container instanceof Container)){
+		return;
+	}
+	if (1 || container.displayType === undefined){
+		container.setDisplayType('maximized');
+	}
+	/*if(['maximized', 'unmaximized', 'unmaximized-pinned'].indexOf(container.displayType.get()) == -1){
+		container.displayType.setValue('maximized');
+		displayType = 'maximized';
+	}
+	else{
+		displayType = container.displayType.get();
+	}
+
+	if(displayType === 'unmaximized' || displayType === 'unmaximized-pinned'){
+		if(typeof this.activeUnmaximizedContainers === 'undefined'){
+			this.activeUnmaximizedContainers = [];
+			this.topZIndex = 200;
+		}
+		this.activeUnmaximizedContainers.push(container);
+
+	}
+
+	if(this.activeContainer == undefined){
+		this.switchTo(container);
+	}
+	else{
+		container.hide();
+	}
+
+	container.parent = this;
+	if(typeof container.windowMode === 'undefined'){
+		container.windowMode = 'extended';
+	}
+	if(container.windowMode === 'unmaximized'){
+
+	}
+	else{
+		this.containers.push(container);
+		if(this.containers.length == 1){
+			this.switchTo(container);
+		}
+		else{
+			container.hide();
+		}
+	}*/
+	this.containers.push(container);
+	container.e.style.visibility = "none";
+
+	Container.prototype.append.call(this, container);
+	if(this.activeContainer == undefined){
+		//container.e.style.visibility = "";
+		this.switchTo(container);
+	}
+	else{
+		container.hide();
+	}
+	container.e.style.visibility = "";
+	/*container.on('closed',function(){
+		var i = self.containers.indexOf(container);
+		self.containers.splice(i,1);
+		if(self.activeContainer == container){
+			if(i == 0){
+				if(self.containers.length > 0){
+					self.switchTo(self.containers[0]);
+				}
+			}
+			else{
+				self.switchTo(self.containers[i-1]);
+			}
+		}
+		container.removeEventListener
+	});*/
+	this.dispatchEvent('container-added', {
+		container:container,
+	});
+};
 
